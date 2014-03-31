2f6b47a1d2416812bb07e501b0e66481484276a9
hhflying <hhflying@gmail.com>
2014-03-31 10:37:54 +0800

    add subdir include/

---
+++ include/dummy.h
@@ -0,0 +1,14 @@
+/**
+ *  @filename: dummy.h
+ *  @author: hhflying (hhflying@gmail.com) 
+ *  @date: 2014-03-31
+ *  @desc: ...
+ *
+ */
+
+#ifndef __DUMMY_H__
+#define __DUMMY_H__
+
+
+#endif //__DUMMY_H__
+
